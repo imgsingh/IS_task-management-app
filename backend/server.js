cf8262ab3fc467ca8cf72bb6d9c928b8c89241be
Gursimran Singh <gursimranbasra7.gs@gmail.com>
2025-03-09 21:32:07 +0000

    added cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3001',
+    origin: ['http://localhost:3001', 'https://is-task-management-app-frontend.vercel.app'],
     credentials: true,
 }));
 app.use(express.json());
