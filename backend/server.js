6dbf8b9975e6d8698ab30125acb4db55e60deecc
Gursimran Singh <gursimranbasra7.gs@gmail.com>
2025-03-13 23:00:39 +0000

    url change

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //Middleware
-app.use(cors({
-    origin: 'http://localhost:3000', //Development
-    credentials: true,
-}));
 // app.use(cors({
-//     origin: 'https://is-task-management-app-frontend.vercel.app', // Production origin
+//     origin: 'http://localhost:3000', //Development
 //     credentials: true,
 // }));
+app.use(cors({
+    origin: 'https://is-task-management-app-frontend.vercel.app', // Production origin
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    apiUrl: 'http://localhost:5000', // Replace with your actual API domain
-    //apiUrl: 'https://is-task-management-app.vercel.app'
+    //apiUrl: 'http://localhost:5000', // Replace with your actual API domain
+    apiUrl: 'https://is-task-management-app.vercel.app'
 };
 
 export default config;
